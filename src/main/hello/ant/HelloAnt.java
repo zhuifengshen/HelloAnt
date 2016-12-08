c771272ce488c5bf8223c9c1505cb3046a33e378
Devin <1324556701@qq.com>
2016-12-08 20:16:57 +0800

    Initial commit

---
+++ src/main/hello/ant/HelloAnt.java
@@ -0,0 +1,13 @@
+package main.hello.ant;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Devin     Date: 2016/12/8 10:29
+ * More wonderful content please pay attention to -> http://zhangchuzhao.site
+ */
+
+public class HelloAnt {
+    public static void main(String[] args) {
+        System.out.println("Hello World From Ant");
+    }
+}
